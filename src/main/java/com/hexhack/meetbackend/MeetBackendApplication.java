b628e9b79e22ab8ea4d69b0a1f18e3cb7f688c73
akashKarmakar02 <akash.anan03@gmail.com>
2023-09-14 19:44:37 +0530

    initial commit

---
+++ src/main/java/com/hexhack/meetbackend/MeetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hexhack.meetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hexhack/meetbackend/MeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexhack.meetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
