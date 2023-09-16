ee05c4c360557e62ad97b8c6378b8a0c548d6a14
akashKarmakar02 <akash.anan03@gmail.com>
2023-09-16 08:56:45 +0530

    Teacher Repo

---
+++ src/main/java/com/hexhack/meetbackend/repository/TeacherRepository.java
@@ -0,0 +1,10 @@
+package com.hexhack.meetbackend.repository;
+
+import com.hexhack.meetbackend.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Optional<Teacher> findByEmail(String email);
+}
